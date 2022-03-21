019d2f24cc409fa2d0970771a25320da3519e69c
appsaprinsky <app1.n.saprinsky@gmail.com>
2022-03-21 20:12:13 +0600

    New

--- rest.py
+++ rest.py
@@ -8,25 +8,11 @@ if __name__ == '__main__':
 ####Register
 
     # URL = "http://localhost:4001/register"
-    # params = {
-    #     "first_name": "niki4",    
-    #     "last_name": "miki4",
-    #     "email": "niki4@gmail.com",    
-    #     "password": "NukeNuke"   
-
-
-    # }
-    
-    
-    # resp = requests.post(URL, json=params)
-    # print(resp.text)
-
-
-
-####Login
-    URL = "http://localhost:4001/login"
+    URL = "https://nodejs-deploy-vercel.vercel.app/register"
     params = {
-        "email": "niki4@gmail.com",    
+        "first_name": "niki5",    
+        "last_name": "miki5",
+        "email": "niki5@gmail.com",    
         "password": "NukeNuke"   
     }
     
@@ -35,13 +21,32 @@ if __name__ == '__main__':
     print(resp.text)
 
 
+
+####Login
+    # URL = "http://localhost:4001/login"
+    # URL = "https://nodejs-deploy-vercel.vercel.app/login"
+    # params = {
+    #     "email": "niki4@gmail.com",    
+    #     "password": "NukeNuke"   
+    # }
+    
+    
+    # resp = requests.post(URL, json=params)
+    # print(resp.text)
+
+
+
+
+
+
 ####Welcome
     # URL = "http://localhost:4001/welcome"
+    # URL = "https://nodejs-deploy-vercel.vercel.app/welcome"
 
-    # # params = {
-    # #     "email": "niki@gmail.com",    
-    # #     "password": "NukeNuke"           
-    # # }
+    # params = {
+    #     "email": "niki@gmail.com",    
+    #     "password": "NukeNuke"           
+    # }
     # headers = {
     #     "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjIzODZlNDJlMGQzMmQzNzBhZTg1ZjNkIiwiZW1haWwiOiJuaWtpNEBnbWFpbC5jb20iLCJpYXQiOjE2NDc4NjU1NjcsImV4cCI6MTY0Nzg3Mjc2N30.J5_E8SmituarH63ag9J84px4w9SHsZN2ULZlkIcC-y0"  
     # }
@@ -50,7 +55,5 @@ if __name__ == '__main__':
     # resp = requests.post(URL, headers=headers)
     # print(resp.text)
     # print(resp.status_code)
-
-
 
 
